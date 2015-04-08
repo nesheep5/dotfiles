a879674eda7b5c733874e2b3d698b4736e45fd0a
Shogo Watanabe <ne.sheep.5mouths@gmail.com>
2015-04-09 08:21:47 +0900

    不要なフォルダを削除

--- dotfiles/dotfilesLink.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-ln -s ~/dotfiles/.gitconfig ~/.gitconfig
-ln -s ~/dotfiles/.gitignore_global ~/.gitignore_global
-ln -s ~/dotfiles/.vim ~/.vim
-ln -s ~/dotfiles/.vimrc ~/.vimrc
-ln -s ~/dotfiles/.zshenv ~/.zshenv
-ln -s ~/dotfiles/.zshrc ~/.zshrc
