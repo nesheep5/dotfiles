f022792f9250d0bf803f47dc4ddb9cda183e0779
Shogo Mizuno <ne.sheep.5mouths@gmail.com>
2020-04-24 15:11:32 +0900

    Update fish.

--- fish/config.fish
+++ fish/config.fish
@@ -7,16 +7,21 @@ set -x PATH  /usr/local/opt/grep/libexec/gnubin $PATH
 
 # set -x BUNDLE_GEMFILE "Gemfile.local"
 
+
 # alias ++++++++++++++++++++++++++++++++++++++++++++++++++
 alias vim="nvim"
 alias vc 'vim ~/.config/nvim/init.vim'
 alias fc 'vim ~/.config/fish/config.fish'
 alias fc_local 'vim ~/.config/fish/config_local.fish'
-alias gl 'ghq look (ghq list | fzf)'
+alias tc 'vim ~/.tmux.conf'
+alias gl 'cd (ghq root) | cd (ghq list | fzf)'
 alias rm 'rmtrash'
-alias gc 'git checkout'
 
-# for Go
+alias gc 'git checkout'
+alias gfu 'git fetch upstream'
+
+alias ide 'tmux split-window -v -p 30; tmux split-window -h -p 66; tmux split-window -h -p 50'
+
 set -x GOPATH $HOME/git
 set -x PATH $PATH $GOPATH/bin
 set -x GOENV_DISABLE_GOPATH 1 # goenvのvar毎にGOPATH管理する機能を無効化
@@ -33,3 +38,5 @@ set -g fish_user_paths "/usr/local/opt/mysql@5.7/bin" $fish_user_paths
 # local config
 source ~/.config/fish/config_local.fish
 
+set -g fish_user_paths "/usr/local/opt/avr-gcc@8/bin" $fish_user_paths
+# set -x PATH $HOME/.local/share/vim-lsp-settings/servers $PATH
